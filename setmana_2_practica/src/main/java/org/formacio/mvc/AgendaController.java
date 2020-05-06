d87a5c1db43461a556afef0656d1ad08fc37696e
Lluismi <lmorey@cifpfbmoll.eu>
2020-05-06 14:57:21 +0200

    Añadido metodo afegir para el test_nou_contacte

--- setmana_2_practica/src/main/java/org/formacio/mvc/AgendaController.java
+++ setmana_2_practica/src/main/java/org/formacio/mvc/AgendaController.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -38,5 +39,11 @@ public class AgendaController {
 			return new ResponseEntity<Persona>(persona, HttpStatus.NOT_FOUND);
 		} else 		
 			return new ResponseEntity<Persona>(persona, HttpStatus.OK);
+	}
+	
+	@RequestMapping(path="/afegir", method=RequestMethod.POST)
+	@ResponseBody
+	public void afegir(@RequestParam String id, String nom, String telefon) {
+		agenda.inserta(id, nom, telefon);
 	}
 }
