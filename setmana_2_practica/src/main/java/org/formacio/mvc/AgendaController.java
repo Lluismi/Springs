7c19cd5afdd35591f1b93f994bf48025e89ca564
Lluismi <lmorey@cifpfbmoll.eu>
2020-05-06 14:25:41 +0200

    Añadido metodo para test_contacte_customitzat y test_contacte

--- setmana_2_practica/src/main/java/org/formacio/mvc/AgendaController.java
+++ setmana_2_practica/src/main/java/org/formacio/mvc/AgendaController.java
@@ -1,8 +1,10 @@
 package org.formacio.mvc;
 
 import org.formacio.repositori.AgendaService;
+import org.formacio.repositori.Persona;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -23,5 +25,11 @@ public class AgendaController {
 	@ResponseBody
 	public String telefon(@RequestParam String id) {
 		return agenda.recupera(id).getTelefon();
+	}
+	
+	@RequestMapping(path = "/contacte/{id}")
+	@ResponseBody
+	public Persona contacte(@PathVariable String id) {
+		return agenda.recupera(id);
 	}
 }
