59848af7c9c093f41d87f815a0d1d4b7476d549a
Lluismi <lmorey@cifpfbmoll.eu>
2020-05-06 14:20:45 +0200

    Creado metodo telefon para test_obte_telefon

--- setmana_2_practica/src/main/java/org/formacio/mvc/AgendaController.java
+++ setmana_2_practica/src/main/java/org/formacio/mvc/AgendaController.java
@@ -4,6 +4,7 @@ import org.formacio.repositori.AgendaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -16,5 +17,11 @@ public class AgendaController {
 	@ResponseBody
 	public String name() {
 		return Integer.toString(agenda.nombreContactes());
+	}
+	
+	@RequestMapping(path = "/telefon")
+	@ResponseBody
+	public String telefon(@RequestParam String id) {
+		return agenda.recupera(id).getTelefon();
 	}
 }
