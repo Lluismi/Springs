4cb3516f813fe9c3de400fb48217311bf5bbd3ac
Lluismi <lmorey@cifpfbmoll.eu>
2020-05-06 14:41:34 +0200

    Añadida bien la conversion XML

--- setmana_2_practica/src/main/java/org/formacio/repositori/Persona.java
+++ setmana_2_practica/src/main/java/org/formacio/repositori/Persona.java
@@ -4,7 +4,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
 
-@JacksonXmlRootElement()
+@JacksonXmlRootElement(localName = "persona")
 public class Persona {
 
 	private String clau;
