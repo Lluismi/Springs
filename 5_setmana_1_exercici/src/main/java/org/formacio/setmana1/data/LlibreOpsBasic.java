8156e5e1e404dce54b0898815c395a2a6a80b340
Lluismi <lmorey@cifpfbmoll.eu>
2020-05-14 18:56:21 +0200

    Creado lógica de los metodos carrega y alta

--- 5_setmana_1_exercici/src/main/java/org/formacio/setmana1/data/LlibreOpsBasic.java
+++ 5_setmana_1_exercici/src/main/java/org/formacio/setmana1/data/LlibreOpsBasic.java
@@ -1,26 +1,49 @@
 package org.formacio.setmana1.data;
 
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+
 import org.formacio.setmana1.domini.Llibre;
 import org.formacio.setmana1.domini.Recomanacio;
+import org.springframework.stereotype.Component;
 
 /**
  * Modifica aquesta classe per tal que sigui un component Spring que realitza les 
  * operacions de persistencia tal com indiquen les firmes dels metodes
  */
+@Component
 public class LlibreOpsBasic {
+	
+	@PersistenceContext
+	private EntityManager em;
 	
 	/**
 	 * Retorna el llibre amb l'ISBN indicat o, si no existeix, llança un LlibreNoExisteixException
 	 */
 	public Llibre carrega (String isbn) throws LlibreNoExisteixException {
-		return null;
+		Llibre exemplar = em.find(Llibre.class, isbn);
+		if(exemplar != null){ 
+			return exemplar;
+		}
+		else {
+			throw new LlibreNoExisteixException();	
+		}
 	}
 	
 	/**
 	 * Sense sorpreses: dona d'alta un nou llibre amb les propietats especificaques
 	 */
+	@Transactional
 	public void alta (String isbn, String autor, Integer pagines, Recomanacio recomanacio, String titol) {
+		Llibre exemplar = new Llibre();
+		exemplar.setIsbn(isbn);
+		exemplar.setAutor(autor);
+		exemplar.setPagines(pagines);
+		exemplar.setRecomanacio(recomanacio);
+		exemplar.setTitol(titol);
+		em.persist(exemplar);
 	}
 	
 	/**
