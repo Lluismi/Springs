fa560f087a1beb4b7c89ed7d793799c6d894ef0e
Lluismi <lmorey@cifpfbmoll.eu>
2020-05-28 10:41:48 +0200

    Añadido metodo hashCode

--- 10_setmana_2_exercici/src/main/java/org/formacio/setmana2/domini/Alumne.java
+++ 10_setmana_2_exercici/src/main/java/org/formacio/setmana2/domini/Alumne.java
@@ -47,4 +47,11 @@ public class Alumne {
 		return true;
 	}
 	
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((nom == null) ? 0 : nom.hashCode());
+		return result;
+	}
+	
 }
